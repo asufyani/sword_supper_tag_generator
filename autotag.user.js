3b04c1b0b69c3d88010186e6ec865e5ff1e4b374
Amir Sufyani <amir.sufyani@gmail.com>
2025-08-19 15:37:37 -0400

    version

--- autotag.user.js
+++ autotag.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Supper Autotag
-// @version      2025-08-19
+// @version      2025-08-19 15:37
 // @description  generate copy/pasteable tags for sword and supper missions
 // @author       u/Thats_a_movie (github.com/asufyani)
 // @match        https://*.devvit.net/index.html*
